9227d18e48a642b310e2c900ebaa24506f13cbd9
Eirik <34853849+eirikgund@users.noreply.github.com>
2024-08-25 08:20:25 +0200

    Add files via upload

---
+++ installerapper.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Oppdatert systemet først:
+sudo pacman -Syu
+
+# Installer base-devel og git for å kunne bygge yay.
+sudo pacman -S base-devel git --noconfirm
+
+# Lag en midlertidig mappe:
+mkdir /tmp/yay
+cd /tmp/yay
+
+# Hent data fra AUR git:
+git clone https://aur.archlinux.org/yay.git
+cd yay
+
+# "Compiler pakken"
+makepkg -si
+
+# Slett midlertidig mappe:
+rm -rf /tmp/yay
+
+# Skriv yay versjon:
+#yay --version
+
+echo !!!VELG liblphobos og ldc!!!
+# Installer onedrive pakke:
+yay -S onedrive-abraunegg
+
+# Installer forskjellige nyttige programmer 
+sudo pacman -S firefox obsidian blender inkscape --noconfirm
+
+echo Resyncing onedrive
+onedrive --resync
+echo Speiler onedrive
+onedrive --synchronize --sync-shared-folders --verbose
+
