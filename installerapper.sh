1430dae49368430f62618f244742d1bfeae58178
Eirik <34853849+eirikgund@users.noreply.github.com>
2024-08-29 14:05:06 +0200

    Update installerapper.sh

--- installerapper.sh
+++ installerapper.sh
@@ -23,15 +23,15 @@ rm -rf /tmp/yay
 # Skriv yay versjon:
 #yay --version
 
-echo !!!VELG liblphobos og ldc!!!
+echo "!!!VELG liblphobos og ldc!!!"
 # Installer onedrive pakke:
 yay -S onedrive-abraunegg
 
 # Installer forskjellige nyttige programmer 
-sudo pacman -S firefox obsidian blender inkscape --noconfirm
+sudo pacman -S firefox obsidian blender inkscape eog libreoffice-fresh okular pdfarranger --noconfirm
+#qemu-full samba dersom maskinen skal være host.
 
-echo Resyncing onedrive
+echo "Resyncing onedrive"
 onedrive --resync
 echo Speiler onedrive
 onedrive --synchronize --sync-shared-folders --verbose
-
