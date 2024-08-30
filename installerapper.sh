1ad554f570aa32790b1dea2a510b89d4a135fa7f
Eirik <34853849+eirikgund@users.noreply.github.com>
2024-08-30 22:25:57 +0200

    Update installerapper.sh

--- installerapper.sh
+++ installerapper.sh
@@ -28,7 +28,7 @@ echo "!!!VELG liblphobos og ldc!!!"
 yay -S onedrive-abraunegg
 
 # Installer forskjellige nyttige programmer 
-sudo pacman -S firefox obsidian blender inkscape eog libreoffice-fresh okular pdfarranger --noconfirm
+sudo pacman -S firefox obsidian blender inkscape eog libreoffice-fresh okular pdfarranger waybar wofi --noconfirm
 #qemu-full samba dersom maskinen skal være host.
 
 echo "Resyncing onedrive"
